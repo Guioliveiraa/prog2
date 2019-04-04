88d1df03a02372250af79af662687cd31209d976
Guioliveiraa <43076769+Guioliveiraa@users.noreply.github.com>
2019-04-04 19:24:20 -0300

    Add files via upload

--- pot.c
+++ pot.c
@@ -5,20 +5,33 @@ negativos (base e expoente) passados como parâmetros e retorna este valor.*/
 int potencia(int base, int exp);
 int main(){
 	int base, exp;
+	
 	printf("Entre com um valor base:");
 	scanf("%d", &base);
 	printf("Entre com um valor exp:");
 	scanf("%d", &exp);
 	
 	potencia(base, exp);
+	if(exp==0){
+			pot=1;
+		}
+		
 	printf("%d", pot);
 	return 0;
-	
 }
 int potencia(int base, int exp){
+	pot=1;
 	
-	
-	pot=base*exp;
+	if(exp==0){
+		pot=1;
+	}
+	if(exp!=0){
+	do{
+	    pot *=base;
+		exp--;
+	}
+	while(exp>0);
+}
 	return pot;
 }
 
